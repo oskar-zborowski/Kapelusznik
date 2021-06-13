26cbc1ba54f3265aa8e2177a490c5ee81c50e94c
1609m <elrohir0234@gmail.com>
2021-06-13 20:57:33 +0200

    ikonki, male zmiany

--- public/assets/js/regLinkCreator.js
+++ public/assets/js/regLinkCreator.js
@@ -1,4 +1,4 @@
 $('.regLink').each(function() {
     let link = $(this).text();
-    $(this).contents().replaceWith('<a class="text-white visible" href="agreement/'+link+'</a>');
+    $(this).contents().replaceWith('<a class="text-white visible" target="_blank" href="agreement/'+link+'</a>');
 });
